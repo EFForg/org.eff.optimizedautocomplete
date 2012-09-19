7e28f205c932e64dad6dd74cc58b64ed08d788f8
Micah Lee <micahflee@riseup.net>
2012-09-19 17:06:23 +0100

    fixed problem where some deleted contacts were getting listed in dropdown. also, changed indenting to 2 chars.

--- optimizedautocomplete.php
+++ optimizedautocomplete.php
@@ -3,101 +3,101 @@
 require_once 'optimizedautocomplete.civix.php';
 
 function optimizedautocomplete_civicrm_contactListQuery( &$query, $name, $context, $id ) {
-    // if other params are passed, the return without any changes
-    if($_GET['context'] != 'navigation' 
-        && $_GET['context'] != 'softcredit'
-        && $_GET['context'] != 'relationship') {
-        return;
-    }
+  // if other params are passed, the return without any changes
+  if($_GET['context'] != 'navigation' 
+    && $_GET['context'] != 'softcredit'
+    && $_GET['context'] != 'relationship') {
+    return;
+  }
 
-    // create temp table for storing result set
-    $randomNum = md5( uniqid( ) );
-    $quickSearchTempTable = "civicrm_temp_quick_search_{$randomNum}";
-    $sql = "CREATE TEMPORARY TABLE {$quickSearchTempTable} ( 
-            contact_id int unsigned,
-            data varchar(255),
-            PRIMARY KEY ( contact_id )
-        ) ENGINE=MyISAM DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci
-        ";
-        
-    CRM_Core_DAO::executeQuery( $sql );
+  // create temp table for storing result set
+  $random_num = md5(uniqid());
+  $quick_search_temp_table = "civicrm_temp_quick_search_{$random_num}";
+  $sql = "
+  CREATE TEMPORARY TABLE {$quick_search_temp_table} ( 
+    contact_id int unsigned,
+    data varchar(255),
+    PRIMARY KEY ( contact_id )
+  ) ENGINE=MyISAM DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci";
+      
+  CRM_Core_DAO::executeQuery($sql);
 
-    // find the match from contact table
+  // find the match from contact table
+  $sql = "
+    SELECT id, sort_name
+    FROM civicrm_contact 
+    WHERE sort_name LIKE '$name%'
+    AND is_deleted = 0
+    ORDER BY sort_name LIMIT 0, 25";
+  
+  $dao = CRM_Core_DAO::executeQuery($sql);
+  $result = array();
+  while($dao->fetch()) {
+    $result[$dao->id] = $dao->sort_name;
+  }
+  
+  // if matches found less than 10, try to match from email table
+  if($dao->N < 10) {
+    // find the match from email table
     $sql = "
-        SELECT id, sort_name
-        FROM civicrm_contact 
-        WHERE sort_name LIKE '$name%'
-        AND is_deleted = 0
-        ORDER BY sort_name LIMIT 0, 25";
-    
-    $dao = CRM_Core_DAO::executeQuery( $sql );
-    $result = array();
-    while ( $dao->fetch() ) {
-        $result[$dao->id] = $dao->sort_name;
-    }
-    
-    // if matches found less than 10, try to match from email table
-    if ( $dao->N < 10 ) {
-        // find the match from email table
-        $sql = "
-            SELECT contact_id, email
-            FROM civicrm_email 
-            WHERE email LIKE '$name%'
-            ORDER BY email LIMIT 0, 25";
-    
-        $dao = CRM_Core_DAO::executeQuery( $sql );
-        while ( $dao->fetch() ) {
-            if ( !array_key_exists( $dao->contact_id, $result ) ) {
-                $result[$dao->contact_id] = $dao->email;
-            }
-        }
-    }
+      SELECT civicrm_contact.id, civicrm_email.email
+      FROM civicrm_contact, civicrm_email 
+      WHERE civicrm_contact.id = civicrm_email.contact_id
+      AND civicrm_email.email LIKE '$name%'
+      AND civicrm_contact.is_deleted = 0
+      ORDER BY civicrm_email.email LIMIT 0, 25";
 
-    // find email addresses, cities for these contacts, and filter out deleted ones
-    $contacts = array();
-    $contact_details = array();
-    $ids = array();
-    foreach($result as $id => $value) { $ids[] = $id; }
-    $ids = implode(", ", $ids);
-    $sql = "
-        SELECT civicrm_contact.id, civicrm_contact.sort_name, civicrm_email.email, civicrm_address.city 
-        FROM civicrm_contact, civicrm_email, civicrm_address 
-        WHERE civicrm_contact.id = civicrm_email.contact_id 
-        AND civicrm_contact.id = civicrm_address.contact_id 
-        AND civicrm_contact.is_deleted = 0
-        AND civicrm_email.is_primary = 1 
-        AND civicrm_address.is_primary = 1 
-        AND civicrm_contact.id IN ( $ids ) 
-        ORDER BY civicrm_contact.sort_name";
-    $dao = CRM_Core_DAO::executeQuery( $sql );
+    $dao = CRM_Core_DAO::executeQuery($sql);
     while($dao->fetch()) {
-        $contact_details[$dao->id] = $dao->sort_name." :: ".$dao->email." :: ".$dao->city;
+      if(!array_key_exists( $dao->contact_id, $result)) {
+        $result[$dao->id] = $dao->email;
+      }
     }
-    foreach($result as $id => $value) {
-        if(isset($contact_details[$id])) {
-            $contacts[$id] = $contact_details[$id];
-        } else {
-            $contacts[$id] = $value;
-        }
-    }
+  }
 
-    // insert into temp table
-    if ( !empty( $contacts ) ) {
-        foreach( $contacts as $cid => $value ) {
-            $value = addslashes($value);
-            $insertSQL[] = "( {$cid}, '{$value}' )";
-        }
-        $insertValues = implode( ',', $insertSQL );
-        $sql = "INSERT INTO {$quickSearchTempTable}
-                (contact_id, data )
-                VALUES
-                {$insertValues}
-                ";
-        CRM_Core_DAO::executeQuery( $sql );
+  // find email addresses, cities for these contacts, and filter out deleted ones
+  $contacts = array();
+  $contact_details = array();
+  $ids = array();
+  foreach($result as $id => $value) { $ids[] = $id; }
+  $ids = implode(", ", $ids);
+  $sql = "
+    SELECT civicrm_contact.id, civicrm_contact.sort_name, civicrm_email.email, civicrm_address.city 
+    FROM civicrm_contact, civicrm_email, civicrm_address 
+    WHERE civicrm_contact.id = civicrm_email.contact_id 
+    AND civicrm_contact.id = civicrm_address.contact_id 
+    AND civicrm_contact.is_deleted = 0
+    AND civicrm_email.is_primary = 1 
+    AND civicrm_address.is_primary = 1 
+    AND civicrm_contact.id IN ( $ids ) 
+    ORDER BY civicrm_contact.sort_name";
+  $dao = CRM_Core_DAO::executeQuery($sql);
+  while($dao->fetch()) {
+    $contact_details[$dao->id] = $dao->sort_name." :: ".$dao->email." :: ".$dao->city;
+  }
+  foreach($result as $id => $value) {
+    if(isset($contact_details[$id])) {
+      $contacts[$id] = $contact_details[$id];
+    } else {
+      $contacts[$id] = $value;
     }
+  }
 
-    // return final query
-    $query = "SELECT data, contact_id as id FROM {$quickSearchTempTable}";
+  // insert into temp table
+  if(!empty($contacts)) {
+    foreach( $contacts as $cid => $value ) {
+      $value = addslashes($value);
+      $insertSQL[] = "( {$cid}, '{$value}' )";
+    }
+    $insertValues = implode( ',', $insertSQL );
+    $sql = "INSERT INTO {$quick_search_temp_table}
+      (contact_id, data)
+      VALUES {$insertValues}";
+    CRM_Core_DAO::executeQuery($sql);
+  }
+
+  // return final query
+  $query = "SELECT data, contact_id as id FROM {$quick_search_temp_table}";
 }
 
 /**
